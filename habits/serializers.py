f1b0bb86e51ac71e11c25b89441297abbb905d4f
Lana Virina <lana.virina@gmail.com>
2025-02-13 14:04:45 +0300

    Create validators

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
 from .models import Habit
-from .validators import validate_duration, validate_regularity, validate_associated_habit, validate_is_pleasurable
+from .validators import validate_duration, validate_regularity, AssociatedHabitValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
-    regularity = serializers.IntegerField(validators=[validate_regularity])
-    duration = serializers.DurationField(validators=[validate_duration])
-    associated_habit = serializers.PrimaryKeyRelatedField(
-        queryset=Habit.objects.all(),
-        validators=[validate_associated_habit]
-    )
-    # is_pleasurable = serializers.BooleanField(validators=[validate_is_pleasurable])
+    regularity = serializers.IntegerField(validators=[validate_regularity], required=False)
+    duration = serializers.DurationField(validators=[validate_duration], required=False)
+    associated_habit = serializers.PrimaryKeyRelatedField(queryset=Habit.objects.all(), allow_empty=True, required=False)
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    validators = [AssociatedHabitValidator('is_pleasurable', 'associated_habit', 'reward')]
+
+    def validate(self, data):
+        if data.get('is_pleasurable') is False and (data.get('regularity') is None or data.get('duration') is None):
+            raise serializers.ValidationError(
+                "Для полезной привычки обязательны для заполнения поля 'regularity' и 'duration'.")
+        return data
 
     class Meta:
         model = Habit
-        exclude = ('user',)
+        fields = '__all__'
--- habits/validators.py
+++ habits/validators.py
@@ -2,8 +2,6 @@ from datetime import timedelta
 
 from rest_framework.serializers import ValidationError
 
-from habits.models import Habit
-
 
 def validate_duration(value):
     if value > timedelta(seconds=120):
@@ -15,20 +13,27 @@ def validate_regularity(value):
         raise ValidationError('Периодичность привычки не может быть реже чем 1 раз в день/ 1 раз в неделю')
 
 
-def validate_associated_habit(value):
-    habit = Habit.objects.all().filter(pk=value.pk).first()
-    if habit and not habit.is_pleasurable:
-        raise ValidationError(
-            'В связанные привычки могут попадать только привычки с признаком приятной привычки.')
+class AssociatedHabitValidator:
 
+    def __init__(self, is_pleasurable, associated_habit, reward):
+        self.is_pleasurable = is_pleasurable
+        self.associated_habit = associated_habit
+        self.reward = reward
 
-# def validate_is_pleasurable(value, data):
-#     if value and data['reward']:
-#         raise ValidationError(
-#             'У приятной привычки не может быть вознаграждения или связанной привычки.'
-#         )
-#
-#
-# def validate_reward(value):
-#     """Исключить одновременный выбор связанной привычки и указания вознаграждения."""
-#     pass
+    def __call__(self, value):
+        associated_habit = value.get(self.associated_habit)
+        is_pleasurable = value.get(self.is_pleasurable)
+        reward = value.get(self.reward)
+
+        if associated_habit and not associated_habit.is_pleasurable:
+            raise ValidationError(
+                'В связанные привычки могут попадать только привычки с признаком приятной привычки.'
+            )
+        if is_pleasurable and (associated_habit or reward):
+            raise ValidationError(
+                'У приятной привычки не может быть связанной привычки и вознаграждения.'
+            )
+        if reward and associated_habit:
+            raise ValidationError(
+                'Можно выбрать либо вознаграждение, либо связанную привычку'
+            )
