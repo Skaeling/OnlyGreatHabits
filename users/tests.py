6fb07ffa0af928350c0b901fc007cd7509bf23d4
Lana Virina <lana.virina@gmail.com>
2025-02-19 20:04:18 +0300

    +tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class UserTest(APITestCase):
+
+    def test_user_create(self):
+        """Тестирование создания юзера"""
+
+        url = reverse("users:register")
+        data = {"email": "user@email.com", "username": "user", "password": 12345}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.all().count(), 1)
